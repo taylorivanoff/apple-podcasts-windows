ca8caf0b1283537ffd316947afc81482fcc2e8ed
Taylor Ivanoff <taylorivanoff@gmail.com>
2026-08-14 23:47:24 +0200

    Update Apple Podcasts configuration to enable toolbar removal

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ pub fn run() {
         tauri_icloud_base::IcloudAppConfig {
             app_name: "Apple Podcasts",
             url: "https://podcasts.apple.com",
-            remove_toolbar: false,
+            remove_toolbar: true,
         },
         tauri::generate_context!(),
     );
